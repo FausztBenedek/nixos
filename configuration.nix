98e4a4b56d00cd31a7bd24087ac96648e1eac02b
FausztBenedek <fausztb@gmail.com>
2025-11-20 16:51:10 +0100

    ..

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  services.qemuGuest.enable = true;
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
